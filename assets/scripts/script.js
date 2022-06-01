68a14c89ad411b7ed1fb7ac74f63729084c2fe19
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-06-01 15:38:50 -0700

    afinished first function and started function to color hour rows based off current time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,24 @@
 var schedule = document.querySelector(".container");
-var hour = ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var hour = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var elIds = ["#9AM", "#10AM", "#11AM", "#12PM", "#1PM", "#2PM", "#3PM", "#4PM", "#5PM"]
+var timeSlot = ["moment('9:00am', 'h:mma')", "moment('10:00am', 'h:mma')", "moment('11:00am', 'h:mma')", "moment('12:00pm', 'h:mma')", "moment('1:00pm', 'h:mma')", "moment('2:00pm', 'h:mma')", "moment('3:00pm', 'h:mma')", "moment('4:00pm', 'h:mma')", "moment('5:00pm', 'h:mma')",];
 var temp = "test";
 console.log(schedule);
 
+console.log(moment().format("ha"));
+// Makes the row elwments and adds it onto the html doc
 function rowMaker() {
-    for(var i = 0; i < hour.length; i++) {
+    for (var i = 0; i < hour.length; i++) {
         var row = document.createElement("tr");
         var time = document.createElement("th");
         var info = document.createElement("th");
         var saveBtn = document.createElement("th");
 
-        row.classList.add("row");
-        time.classList.add("hour");
-        info.classList.add("description");
-        saveBtn.classList.add("saveBtn");
+        row.classList.add("row", "col-12");
+        time.classList.add("hour", "col-1");
+        info.classList.add("description", "col-10", "future");
+        info.setAttribute("id", hour[i]);
+        saveBtn.classList.add("saveBtn", "col-1");
 
         time.innerHTML = hour[i];
         row.appendChild(time);
@@ -27,4 +32,25 @@ function rowMaker() {
     }
 }
 
+// colors the hours  based off time
+function momentChecker() {
+    for (var i = 0; i < hour.length; i++) {
+        var rowId = $(elIds[i]);
+        var currentTime = moment().format("h:mma");
+        var currentTimeHour = moment().format("ha");
+        if (currentTime < timeSlot[i] ) {
+            console.log("hi");
+            rowId.removeClass("future");
+            rowId.addClass("past");
+        }
+
+        if (currentTimeHour === moment().format("ha")) {
+            rowId.removeClass("future");
+            rowId.removeClass("past");
+            rowId.addClass("present");
+        }
+    }
+}
+
 rowMaker();
+momentChecker()
